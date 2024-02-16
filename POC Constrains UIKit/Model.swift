f879f000990496f75177db11f67401adadc1ecb6
Keyhou <31210145+Keyhou@users.noreply.github.com>
2024-02-16 17:19:32 +0100

    Finished

---
+++ POC Constrains UIKit/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  POC Constrains UIKit
+//
+//  Created by Keyhan Mortezaeifar on 14/02/2024.
+//
+
+import Foundation
---
+++ POC Constrains UIKit/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  POC Constrains UIKit
+//
+//  Created by Keyhan Mortezaeifar on 14/02/2024.
+//
+
+import Foundation
